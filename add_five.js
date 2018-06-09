d7e7d5a7d9861a0ec921d30cb51f44ef59449ec3
Stéphane Bisinger <stephane.bisinger@protonmail.com>
2018-06-09 17:56:58 +0200

    Add an actual implementation to add 5

--- add_five.js
+++ add_five.js
@@ -1,5 +1,14 @@
 
-module.exports = (event) => {
-  // TODO: Do something
-  return Promise.resolve(true);
+module.exports = (put, records) => {
+  put.on('error', console.error);
+  records.filter(record => record.eventName == 'INSERT').forEach((record) => {
+    console.log('Processing: %j', record);
+    const value = record.dynamodb.Keys.value;
+    put.write({value: value + 5});
+  });
+  const result = new Promise((resolve, reject) => {
+    put.on("finish", () => resolve('Yay'));
+  });
+  put.end();
+  return result;
 }
--- handler.js
+++ handler.js
@@ -1,10 +1,14 @@
 'use strict';
+const aws = require('aws-sdk');
+const dynamoStreams = require('dynamo-streams');
 
 const add_five = require('./add_five.js');
 
 module.exports = {
   add_five: (event, context, callback) => {
-    add_five(event).then((result) => callback(null, result))
+    const dbStreams = dynamoStreams(new aws.DynamoDB);
+    const put = dbStreams.createPutStream({TableName: "numbers"});
+    const records = event.Records.map(record => aws.DynamoDB.Converter.unmarshall(record));
+    add_five(put, records).then((result) => callback(null, result))
   }
-
 };
